24737af5ad192966bf39e7483e6690e4e4986ba5
Ankur Bhardwaj <ankurb75@gmail.com>
2024-07-05 22:44:35 -0700

    added some basic functions

--- solver.py
+++ solver.py
@@ -1,3 +1,6 @@
+# Sample problem sudoku puzzle board
+# 0 indicates empty spaces
+# Board is in row-wise matrix format
 board = [
     [2,9,0,0,0,0,0,8,7],
     [0,0,0,0,8,0,0,0,0],
@@ -9,3 +12,32 @@ board = [
     [0,0,0,0,9,0,0,0,0],
     [3,1,0,0,0,0,0,9,8],
 ]
+
+# Visualizes the board
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - -")
+        
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end = "")
+            
+            if j == 8:
+                print(str(board[i][j]))
+            else:
+                print(str(board[i][j]) + " ", end = "")
+
+# Returns the (row, col) pair of first empty slot
+# else returns False
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)
+    return False
+
+
+
+# print_board(board)
+# print(find_empty(board))
