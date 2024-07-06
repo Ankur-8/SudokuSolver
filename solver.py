de3a0826e12964985fd7294490380ce8d5b9b924
Ankur Bhardwaj <ankurb75@gmail.com>
2024-07-06 13:41:58 -0700

    added check valid func

--- solver.py
+++ solver.py
@@ -14,29 +14,49 @@ board = [
 ]
 
 # Visualizes the board
-def print_board(board):
-    for i in range(len(board)):
+def print_board(bd):
+    for i in range(len(bd)):
         if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - -")
         
-        for j in range(len(board[0])):
+        for j in range(len(bd[0])):
             if j % 3 == 0 and j != 0:
                 print(" | ", end = "")
             
             if j == 8:
-                print(str(board[i][j]))
+                print(str(bd[i][j]))
             else:
-                print(str(board[i][j]) + " ", end = "")
+                print(str(bd[i][j]) + " ", end = "")
 
 # Returns the (row, col) pair of first empty slot
 # else returns False
-def find_empty(board):
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if board[i][j] == 0:
+def find_empty(bd):
+    for i in range(len(bd)):
+        for j in range(len(bd[0])):
+            if bd[i][j] == 0:
                 return (i, j)
     return False
 
+# Takes a board as input and returns True if valid else False
+# board is sudoku board
+# val is the value we are going to insert
+# pos is the position in form of (row, col)
+def isValid(bd, val, pos):
+    # check row and col
+    for i in range(len(bd)):
+        if bd[pos[0]][i] == val and pos[1] != i:
+            return False
+        if bd[i][pos[1]] == val and pos[0] != i:
+            return False
+    # check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x*3, box_x*3 + 3):
+            if bd[i][j] == val and (i,j) != pos:
+                return False
+    return True
 
 
 # print_board(board)
